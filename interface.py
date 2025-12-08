36c6fe5a9a13f09974037c34484e17c7e80ae445
Jan Kores <jan.kores@mensa.cz>
2025-12-08 11:07:46 +0100

    update

--- interface.py
+++ interface.py
@@ -4,6 +4,7 @@ from alvailableModels import AvailableModels
 
 class Interface(ctk.CTk):
     def __init__(self):
+        """initialization of the basic layout"""
         super().__init__()
         self.simulator= Simulator()
         #####
@@ -12,64 +13,120 @@ class Interface(ctk.CTk):
         self.title("Pharmacokineticks simulator")
         self.geometry("800x500")
         
-        self.label = ctk.CTkLabel(self, text="Availible pharmacokinetic models:")
-        self.label.grid(row=0, column=0, columnspan=2, padx=10, pady=(10, 5), sticky="w")
-
+        self.label = ctk.CTkLabel(self, text="Availible pharmacokinetic models:",font=("Helvetica",14,"bold"))
+        self.label.grid(row=0, column=0, columnspan=2, padx=10, pady=5, sticky="w")
 
         self.chosen_model = ctk.StringVar(value="IntraVenous")
-        self.radio_a = ctk.CTkRadioButton(self, text="Intravenous single compartment model", variable=self.chosen_model, value="IntraVenous")
+        self.radio_a = ctk.CTkRadioButton(self, text="Intravenous single compartment model", variable=self.chosen_model, value="IntraVenous", command=self.show_intravenous_parameters)
         self.radio_a.grid(row=1, column=0, padx=10, pady=5, sticky="w")
-        self.radio_b = ctk.CTkRadioButton(self, text="Per oral single compartment model", variable=self.chosen_model, value="PerOral")
+        self.radio_b = ctk.CTkRadioButton(self, text="Per oral single compartment model", variable=self.chosen_model, value="PerOral", command=self.show_peroral_parameters)
         self.radio_b.grid(row=1, column=1, padx=10, pady=5, sticky="w")
 
-        self.label = ctk.CTkLabel(self, text="Simulation parameters:")
-        self.label.grid(row=2, column=0, columnspan=2, padx=10, pady=(10, 5), sticky="w")
+        self.label = ctk.CTkLabel(self, text="Simulation parameters:", font=("Helvetica",14,"bold"))
+        self.label.grid(row=2, column=0, columnspan=2, padx=10, pady=5, sticky="w")
         
         self.entry_label_1 = ctk.CTkLabel(self, text="Start time [h]:")
-        self.entry_label_1.grid(row=3, column=0, padx=10, pady=(15, 0), sticky="w")
-        self.entry_1 = ctk.CTkEntry(self, width=200, placeholder_text=str(self.simulator.start_t))
+        self.entry_label_1.grid(row=3, column=0, padx=10, pady=5, sticky="w")
+        self.entry_1 = ctk.CTkEntry(self, width=200, placeholder_text="start_t")
+        self.entry_1.insert(0,str(self.simulator.start_t))
         self.entry_1.grid(row=3, column=1, padx=10, pady=5, sticky="w")
 
         self.entry_label_2 = ctk.CTkLabel(self, text="Stop time [h]:")
-        self.entry_label_2.grid(row=4, column=0, padx=10, pady=(15, 0), sticky="w")
-        self.entry_2 = ctk.CTkEntry(self, width=200, placeholder_text=str(self.simulator.stop_t))
+        self.entry_label_2.grid(row=4, column=0, padx=10, pady=5, sticky="w")
+        self.entry_2 = ctk.CTkEntry(self, width=200, placeholder_text="stop_t")
+        self.entry_2.insert(0,str(self.simulator.stop_t))
         self.entry_2.grid(row=4, column=1, padx=10, pady=5, sticky="w")
 
         self.entry_label_3 = ctk.CTkLabel(self, text="Number of steps:")
-        self.entry_label_3.grid(row=5, column=0, padx=10, pady=(15, 0), sticky="w")
-        self.entry_3 = ctk.CTkEntry(self, width=200, placeholder_text=str(self.simulator.nsteps))
+        self.entry_label_3.grid(row=5, column=0, padx=10, pady=5, sticky="w")
+        self.entry_3 = ctk.CTkEntry(self, width=200, placeholder_text="nsteps")
+        self.entry_3.insert(0,str(self.simulator.nsteps))
         self.entry_3.grid(row=5, column=1, padx=10, pady=5, sticky="w")
 
         self.entry_label_4 = ctk.CTkLabel(self, text="Initial dose [mg]:")
-        self.entry_label_4.grid(row=6, column=0, padx=10, pady=(15, 0), sticky="w")
-        self.entry_4 = ctk.CTkEntry(self, width=200, placeholder_text=str(self.simulator.D))
+        self.entry_label_4.grid(row=6, column=0, padx=10, pady=5, sticky="w")
+        self.entry_4 = ctk.CTkEntry(self, width=200, placeholder_text="D")
+        self.entry_4.insert(0,str(self.simulator.D))
         self.entry_4.grid(row=6, column=1, padx=10, pady=5, sticky="w") 
 
         self.entry_label_5 = ctk.CTkLabel(self, text="Volume of distribution [l]:")
-        self.entry_label_5.grid(row=7, column=0, padx=10, pady=(15, 0), sticky="w")
-        self.entry_5 = ctk.CTkEntry(self, width=200, placeholder_text=str(self.simulator.Vd))
+        self.entry_label_5.grid(row=7, column=0, padx=10, pady=5, sticky="w")
+        self.entry_5 = ctk.CTkEntry(self, width=200, placeholder_text="Vd")
+        self.entry_5.insert(0,str(self.simulator.Vd))
         self.entry_5.grid(row=7, column=1, padx=10, pady=5, sticky="w") 
 
         #### rep_ D
         self.cb_option1_var = ctk.BooleanVar(value=self.simulator.rep_D)
         self.entry_label_cb = ctk.CTkLabel(self, text="Repeated dose:")
-        self.entry_label_cb.grid(row=8, column=0, padx=10, pady=(15, 0), sticky="w")
-        self.cb_option1 = ctk.CTkCheckBox(self, text="", variable=self.cb_option1_var)
+        self.entry_label_cb.grid(row=8, column=0, padx=10, pady=5, sticky="w")
+        self.cb_option1 = ctk.CTkCheckBox(self, text="", variable=self.cb_option1_var,command=self.show_hide_rep_dose)
         self.cb_option1.grid(row=8, column=1, padx=10, pady=5, sticky="w")
 
         self.entry_label_6 = ctk.CTkLabel(self, text="Time between doses [h]:")
-        self.entry_label_6.grid(row=9, column=0, padx=10, pady=(15, 0), sticky="w")
-        self.entry_6 = ctk.CTkEntry(self, width=200, placeholder_text=str(self.simulator.dose_int))
+        self.entry_label_6.grid(row=9, column=0, padx=10, pady=5, sticky="w")
+        self.entry_6 = ctk.CTkEntry(self, width=200, placeholder_text="dose_int")
+        self.entry_6.insert(0,str(self.simulator.dose_int))
         self.entry_6.grid(row=9, column=1, padx=10, pady=5, sticky="w") 
 
-        # self.label = ctk.CTkLabel(self, text="Model parameters:")
-        # self.label.grid(row=2, column=0, columnspan=2, padx=10, pady=(10, 5), sticky="w")
+        self.label = ctk.CTkLabel(self, text="Model parameters:", font=("Helvetica",14,"bold"))
+        self.label.grid(row=10, column=0, columnspan=2, padx=10, pady=5, sticky="w")
 
+        self.entry_label_7 = ctk.CTkLabel(self, text="Rate constant of elimination [1/h]:")
+        self.entry_label_7.grid(row=11, column=0, padx=10, pady=5, sticky="w")
+        self.entry_7 = ctk.CTkEntry(self, width=200, placeholder_text="k_a")
+        self.entry_7.insert(0,str(self.simulator.model_params["k_e"]))
+        self.entry_7.grid(row=11, column=1, padx=10, pady=5, sticky="w") 
 
+        self.entry_label_8 = ctk.CTkLabel(self, text="Rate constant of absorption [1/h]:")
+        self.entry_label_8.grid(row=12, column=0, padx=10, pady=5, sticky="w")
+        self.entry_8 = ctk.CTkEntry(self, width=200, placeholder_text="k_a")
+        self.entry_8.insert(0,str(self.simulator.model_params["k_a"]))
+        self.entry_8.grid(row=12, column=1, padx=10, pady=5, sticky="w") 
+        
+        self.remove_params()
+        self.entry_label_6.grid_remove()
+        self.entry_6.grid_remove()
+        self.entry_label_7.grid()
+        self.entry_7.grid()
+        
         run_button = ctk.CTkButton(self, text="Run", command=self.run_button_callback)
-        run_button.grid(row=10, column=1, padx=20, pady=20)
+        run_button.grid(row=100, column=1, padx=20, pady=20)
+
+        self.error_lable = ctk.CTkLabel(self, text="", text_color="red")
+        self.error_lable.grid(row=100, column=2, padx=10, pady=20, sticky="w")
     
+    def remove_params(self):
+        self.entry_7.grid_remove()
+        self.entry_label_7.grid_remove()
+        self.entry_8.grid_remove()
+        self.entry_label_8.grid_remove()
+
+    def show_intravenous_parameters(self):
+        """callback function for showing right parameters for intra venous single compartment model"""
+        self.remove_params()
+        self.entry_7.grid()
+        self.entry_label_7.grid()
+    
+    def show_peroral_parameters(self):
+        """callback function for showing right parameters for per orals single compartment model"""
+        self.remove_params()
+        self.entry_7.grid()
+        self.entry_label_7.grid()
+        self.entry_8.grid()
+        self.entry_label_8.grid()
+
+    def show_hide_rep_dose(self):
+        """callback function for showing/hiding time between doses parameter for repeated dose option"""
+        if self.cb_option1_var.get():
+            self.entry_6.grid()
+            self.entry_label_6.grid()
+        else:
+            self.entry_6.grid_remove()
+            self.entry_label_6.grid_remove()
+
+
     def run_button_callback(self):
+        """callback function for running the simulation"""
         if self.chosen_model.get() == "IntraVenous":
             self.simulator.set_parameters(chosen_model=AvailableModels.intraVenousSC)
         elif self.chosen_model.get() == "PerOral":
@@ -87,10 +144,9 @@ class Interface(ctk.CTk):
                 rep_D=self.cb_option1_var,
                 dose_int = float(self.entry_6.get())
             )
+            self.error_lable.configure(text="")
         except:
-            print("fail")
-            #self.error_lable = ctk.CTkLabel(self, text="Error: wrong type of variable entry!")
-            #self.error_lable.grid(row=100, column=0, padx=10, pady=(15, 0), sticky="w")
+            self.error_lable.configure(text="Error: Wrong type of entry!")
             return
 
 
